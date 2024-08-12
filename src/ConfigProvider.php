0482c38fef420dabab27e866466e940b73040aee
japool@163.com <japool@163.com>
2024-08-12 10:56:21 +0800

    分离生成base 文件

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -34,8 +34,57 @@ class ConfigProvider
                     'id' => 'ApiException',
                     'description' => 'ApiException generate', // 描述
                     // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
-                    'source' => __DIR__ . '/publish/ApiExceptionHandler.php.copy',  // 对应的配置文件路径
+                    'source' => __DIR__ . '/publish/stub/ApiExceptionHandler.stub',  // 对应的配置文件路径
                     'destination' => BASE_PATH . '/app/Middleware/Handler/ApiExceptionHandler.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'JsonCallBackInterFace',
+                    'description' => 'JsonCallBackInterFace generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/JsonCallBackInterface.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/src/JsonCallBackInterface.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'JsonCallBack',
+                    'description' => 'JsonCallBack generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/JsonCallBack.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/src/JsonCallBack.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'RepositoryPackage',
+                    'description' => 'RepositoryPackage generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/RepositoryPackage.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/src/RepositoryPackage.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'BaseService',
+                    'description' => 'BaseService generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/BaseService.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/BaseService.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'BaseRepository',
+                    'description' => 'BaseRepository generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/BaseRepository.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/BaseRepository.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'BaseModel',
+                    'description' => 'BaseModel generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/BaseModel.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/BaseRepository.php', // 复制为这个路径下的该文件
+                ],
+                [
+                    'id' => 'BaseController',
+                    'description' => 'BaseController generate', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/publish/stub/BaseController.stub',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/app/Base/BaseController.php', // 复制为这个路径下的该文件
                 ],
             ],
             // 亦可继续定义其它配置，最终都会合并到与 ConfigInterface 对应的配置储存器中

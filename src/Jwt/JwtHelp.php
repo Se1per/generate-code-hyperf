77d01d94dc310b1b2750d2ce2fa65d8c4fce0d7c
japool@163.com <japool@163.com>
2025-02-21 16:17:47 +0800

    修复接口生成得命名错误问题

--- src/Jwt/JwtHelp.php
+++ src/Jwt/JwtHelp.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Japool\Genconsole\Jwt;
 
+use App\Constants\CodeConstants;
 use DomainException;
 use Exception;
 use Firebase\JWT\BeforeValidException;
@@ -24,41 +25,48 @@ use Firebase\JWT\SignatureInvalidException;
 use Hyperf\Config\Annotation\Value;
 use InvalidArgumentException;
 use UnexpectedValueException;
+use function Hyperf\Support\env;
 
-trait JwtHelp
+class JwtHelp
 {
     #[Value('generator.jwt')]
     private $config;
 
-    public function make(array $userInfo, &$expiresIn = null): string
+    public function make(array $userInfo, &$expiresIn = null,$audUrl = null,$XForwarded = null): array
     {
         if ($expiresIn) {
             $start = time();
             $end = $start + $expiresIn;
+            $exp = $expiresIn;
         } else {
             $start = time();
             $end = $start + $this->config['exp'];
+            $exp = $this->config['exp'];
         }
 
         $expiresIn = $end;
 
         $payload = [
             // 在这个例子中，http://example.org 表示 JWT 是由该 URL 所标识的实体签发的。
-            'iss' => 'http://example.org',
-            // http://example.com 表示这个 JWT 是为该 URL 所标识的实体设计的，通常接收并验证 JWT 的服务应该与这个值匹配
-            'aud' => 'http://example.com',
-            // 这是 JWT 签发的时间戳，表示 JWT 被创建的日期和时间。在这里，1356999524 是一个Unix时间戳，它对应于2012年12月28日的某个时间点
-            'iat' => $start,
-            // 这个声明指定了 JWT 在什么时间之后才有效。1357000000 也是一个Unix时间戳，表示 JWT 在此时间之后才能被接受和处理。如果在 nbf 时间之前尝试使用 JWT，验证将会失败。
-            'nbf' => $start,
-            'exp' => $end,
+            'iss' => env('APP_NAME','http://example.com'),//发行者 (Issuer)
+            'aud' => $audUrl == null ? env('APP_NAME','http://example.com') : $audUrl,//接收者 (Audience)，例如API服务的URL
+            'iat' => $start,  //签发时间 (Issued At)
+            'nbf' => $start, // 生效时间 (Not Before)
+            'exp' => $end,   // 过期时间 (Expiration Time)
+            'sub' => $userInfo['id'], // 主题 (Subject)，例如用户的唯一标识符
             'info' => $userInfo,
         ];
 
-        $headers = $this->config['x-forwarded-for'];
+        $headers = [];
+
+        if($XForwarded){
+            $headers = $XForwarded;
+        }
+
+        $token = JWT::encode($payload, $this->config['secret'],$this->config['algorithm'], null, $headers);
 
         // Encode headers in the JWT string
-        return JWT::encode($payload, $this->config['secret'],$this->config['algorithm'], null, $headers);
+        return [$token,$exp];
     }
 
     public function decodeJwtToken($token): array
@@ -76,35 +84,37 @@ trait JwtHelp
         } catch (InvalidArgumentException $e) {
             // 提供的密钥/密钥数组为空或格式不正确。
 //            throw new SignatureInvalidException($e->getMessage());
-            return [false,300002];
+            return [false,CodeConstants::TOKEN_INVALID];
         } catch (DomainException $e) {
             // 提供的算法不受支持或
             // 提供的密钥无效或
             // openSSL或libsodium or中引发未知错误
             // 需要libsodium，但不可用。
 //            throw new DomainException($e->getMessage());
-            return [false,300002];
+            return [false,CodeConstants::TOKEN_INVALID];
         } catch (SignatureInvalidException $e) {
             // 提供的JWT签名验证失败。
 //            throw new SignatureInvalidException($e->getMessage());
-            return [false,300002];
+            return [false,CodeConstants::TOKEN_INVALID];
+
         } catch (BeforeValidException $e) {
             // 前提是JWT试图在“nbf”索赔或
             // 前提是JWT试图在“iat”索赔之前使用。
 //            throw new BeforeValidException($e->getMessage());
-            return [false,300001];
+            return [false,CodeConstants::TOKEN_TIME_OUT];
+
         } catch (ExpiredException $e) {
             // 前提是JWT试图在“exp”索赔后使用。
-            return [false,300001];
+            return [false,CodeConstants::TOKEN_ILLICIT];
         } catch (UnexpectedValueException $e) {
             // 前提是JWT格式错误或
             // 假设JWT缺少算法/使用了不受支持的算法OR
             // 提供的JWT算法与提供的密钥OR不匹配
             // 在密钥/密钥数组中提供的密钥ID为空或无效。
-            return [false,300002];
+            return [false,CodeConstants::TOKEN_ILLICIT];
         } catch (Exception $e) {  // 其他错误
 //            throw new ExpiredException($e->getMessage());
-            return [false,300003];
+            return [false,CodeConstants::TOKEN_ILLICIT];
         }
 
         return [true,(array) $decoded];
